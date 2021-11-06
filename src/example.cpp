397b077b6fc1cf9e6331d00dbab58232d3e19b3a
mdmubin <md.mubin030@gmail.com>
2021-11-06 10:16:56 +0600

    update cmakelists and example.cpp

--- src/example.cpp
+++ src/example.cpp
@@ -1,8 +1,11 @@
-#include "example.h"
+#include "myexternalheader.h"
 
-#include <iostream>
+// void hello()
+// {
+//     std::cout << "hello, library function!" << std::endl;
+// }
 
-void example::function()
-{
-    std::cout << "hello, library function!" << std::endl;
-}
+// if building executable comment out above and uncomment below
+// int main() {
+//     hello();
+// }
