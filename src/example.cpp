4f987924b6750f4d3ab1c6e7fd37b307f310501f
mdmubin <md.mubin030@gmail.com>
2021-11-06 08:46:15 +0600

    updates

---
+++ src/example.cpp
@@ -0,0 +1,8 @@
+#include "example.h"
+
+#include <iostream>
+
+void example::function()
+{
+    std::cout << "hello, library function!" << std::endl;
+}
---
+++ src/include/example.h
@@ -0,0 +1,9 @@
+#ifndef PROJECT_NAME_EXAMPLE_H
+#define PROJECT_NAME_EXAMPLE_H
+
+namespace example
+{
+    void function();
+}
+
+#endif //PROJECT_NAME_EXAMPLE_H
